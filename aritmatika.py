9c691b05afeb7623f51ff3b3855ef6cf7298aed3
dappunjaya90-source <dappunjaya90@gmail.com>
2025-10-09 15:11:53 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,61 @@
+a = 10
+b = 15
+
+# penambahan
+print(a+b)
+
+# pengurangan
+print(a-b)
+
+# perkalian
+print(a*b)
+
+# pembagian
+print(a/b)
+
+# pembagian pembulatan ke bawah apabila hasilnya positif, pembulatan ke atas apabila hasilnya negative
+print(a//b)
+
+c = 5
+d = 10
+
+# Penjumlahan
+hasil_penjumlahan = c+d
+print("Hasil penjumlahan angka: {}".format(hasil_penjumlahan))
+
+# Pengurangan
+hasil_pengurangan = c-d
+print("Hasil pengurangan angka: {}".format(hasil_pengurangan))
+
+# Perkalian
+hasil_perkalian = c*d
+print("Hasil perkalian angka: {}".format(hasil_perkalian))
+
+# Pembagian
+hasil_pembagian = c/d
+print("Hasil pembagian angka: {}".format(hasil_pembagian))
+
+# Perbandingan
+hasil_perbandingan = c==d
+print("Hasil perbandingan angka: {}".format(hasil_perbandingan))
+
+# Tidak sama dengan
+hasil_tidak_sama_dengan = c!=d
+print("Hasil tidak sama dengan angka: {}".format(hasil_tidak_sama_dengan))
+
+
+# Lebih besar dari
+hasil_lebih_besar_dari = c>d
+print(c>d)
+
+# Lebih kecil dari
+hasil_lebih_kecil_dari = c<d
+print(c<d)
+
+# Lebih besar sama dengan
+hasil_lebih_besar_sama_dengan = c>=d
+print(c>=d)
+
+# Lebih kecil sama dengan
+hasil_lebih_kecil_sama_dengan = c>=d
+print(c<=d)
