bbd63bba1069eea2d7b972142d9aa91b5a318743
dappunjaya90-source <dappunjaya90@gmail.com>
2025-10-09 15:14:21 +0700

    Add files via upload

---
+++ latihanrumus.py
@@ -0,0 +1,7 @@
+# Menghitung luas persegi panjang
+
+panjang = 10
+lebar = 5 
+
+luas_persegi_panjang = panjang * lebar
+print("Luas persegi panjang adalah {}".format(luas_persegi_panjang))
---
+++ latihanrumus2.py
@@ -0,0 +1,8 @@
+# Menghitung luas persegi panjang
+
+panjang = float(input("Masukkan panjang:"))
+lebar = float(input("Masukkan lebar:"))
+
+luas_persegi_panjang = panjang*lebar
+print("Luas persegi panjang adalah {}".format(luas_persegi_panjang))
+
